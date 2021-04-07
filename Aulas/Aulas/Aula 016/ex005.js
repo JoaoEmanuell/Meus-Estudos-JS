f4ba9fa6b605385bf783e01e3e1631acfdd36076
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-04-07 17:12:30 -0300

    txt fatorial recusividade

---
+++ Aulas/Aulas/Aula 016/ex005.js
@@ -0,0 +1,17 @@
+//Recursividade
+function fatorial(x){
+    if (x == 1){
+        return 1
+    }
+    else{
+        return x * fatorial(x - 1)
+    }
+}
+
+console.log(fatorial(5))
+
+/*
+
+De forma geral o fatorial de um numero pode ser calculado a dorma anterior do ex004 ou o fatorial de um numero é igual a ele vezes o fatorial dele - 1
+
+*/
