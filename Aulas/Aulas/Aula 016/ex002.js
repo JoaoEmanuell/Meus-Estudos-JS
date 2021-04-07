b14182edd03fafe669f4b9a48c32415ea5f465f2
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-04-07 16:32:54 -0300

    Parametros opicionais comentario

--- Aulas/Aulas/Aula 016/ex002.js
+++ Aulas/Aulas/Aula 016/ex002.js
@@ -1,4 +1,4 @@
-function soma(n1, n2){
+function soma(n1=0, n2=0){//fazendo com que os paramentos se tornem opcionais mesmo se eu não colocar um deles acaba acontecendo de não bugar
     return n1 + n2
 }
 
