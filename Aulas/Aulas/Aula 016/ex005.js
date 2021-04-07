bb18dd983f27b7ba30c156480d5e2a367d73f95b
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-04-07 17:24:19 -0300

    teste

--- Aulas/Aulas/Aula 016/ex005.js
+++ Aulas/Aulas/Aula 016/ex005.js
@@ -14,4 +14,5 @@ console.log(fatorial(5))
 
 De forma geral o fatorial de um numero pode ser calculado a dorma anterior do ex004 ou o fatorial de um numero é igual a ele vezes o fatorial dele - 1
 
+Teste para verificar a extensão nova
 */
