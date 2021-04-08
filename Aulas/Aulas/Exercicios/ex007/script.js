945aabd7ec18093b529fc495efef2ce4a7f48358
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-04-08 16:18:57 -0300

    função fim criada, arquivo teste criado, fim do exercicio

--- Aulas/Aulas/Exercicios/ex007/script.js
+++ Aulas/Aulas/Exercicios/ex007/script.js
@@ -20,6 +20,7 @@ function inLista(n, l){
 }
 
 function add(){
+    res.innerHTML = ''
     if (isNumber(num.value) && !inLista(num.value, valores)) {
         valores.push(Number(num.value))
         let item = window.document.createElement('option' )
@@ -28,8 +29,48 @@ function add(){
         console.log(item)
         
     }else{
-        window.alert('Valor invalido ou ja encontrado na lista')
+        window.alert('Valor invalido ou foi encontrado na lista')
     }
     num.value = ''
     num.focus()//focus serve para fazer o cursor focar aquela posição
+}
+
+function fim(){
+    if (valores.length == 0){
+        window.alert('Erro, coloque algum numero')
+    }
+    else if (valores.length == 1){
+        res.innerHTML += `<br>Essa lista tem ${valores.length} valor <br><br>`
+        res.innerHTML += `O maior valor é ${valores}<br><br>`
+        res.innerHTML += `Somando os valore temos ${valores}<br><br>`
+        res.innerHTML += `A media dos valores é ${valores}<br><br>`
+    }
+    else{
+        res.innerHTML += `<p>Essa lista tem ${valores.length} valores</p>`
+
+        let maior = valores[0]
+        let menor = valores[0]
+        for (c in valores){
+            if (valores[c] > maior){
+                maior = valores[c]
+            }else if (valores[c] < menor){
+                menor = valores[c]
+            }
+        }
+
+        res.innerHTML += `<p>O maior valor é ${maior}</p>`
+
+        res.innerHTML += `<p>O menor valor é ${menor}</p>`
+
+        let soma = 0
+        for (c in valores){
+            soma += valores[c]
+        }
+
+        res.innerHTML += `<p>A soma entre todos os valores é ${soma}</p>`
+
+        let media = soma / valores.length
+
+        res.innerHTML += `<p>A media entre os valores é ${media}</p>`
+    }
 }
