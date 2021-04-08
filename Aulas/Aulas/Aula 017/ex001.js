79c2bf1361177b804833d9cc013ca91118f1a1a2
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-04-08 20:36:35 -0300

    txt, js demonstrar função dentro de objeto e objeto

--- Aulas/Aulas/Aula 017/ex001.js
+++ Aulas/Aulas/Aula 017/ex001.js
@@ -1,1 +1,10 @@
-let amigo = {nome:'José', sexo:'M', peso:55.7}
+let amigo = {nome:'José',
+ sexo:'M',
+  peso:55.7,
+engordar(p=0){
+    console.log('Engordou')
+    this.peso += p//this é um metodo que faz com que ele se referencie ao propio objeto
+}}
+
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}`)
