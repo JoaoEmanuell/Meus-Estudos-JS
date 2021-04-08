6451000767a22a63919443c1a5e5ff5cbcdcf7c3
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-04-08 15:19:14 -0300

    getElemnts html

---
+++ Aulas/Aulas/Exercicios/ex007/script.js
@@ -0,0 +1,9 @@
+let num = window.document.getElementById('num')
+let lista = window.document.getElementById('lista')
+let res = window.document.getElementById('res')
+var valores = []
+
+
+function add(){
+    
+}
