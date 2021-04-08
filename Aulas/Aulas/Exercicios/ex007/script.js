b7d759abfe242dab597868bb38e0b650418b6144
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-04-08 15:29:05 -0300

    codigo de adição na array e verificação de erros

--- Aulas/Aulas/Exercicios/ex007/script.js
+++ Aulas/Aulas/Exercicios/ex007/script.js
@@ -3,7 +3,26 @@ let lista = window.document.getElementById('lista')
 let res = window.document.getElementById('res')
 var valores = []
 
+function isNumber(n){
+    if (Number(n) >= 1 && Number(n) <= 100)
+    return true
+    else{
+        return false
+    }
+}
+
+function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    } else{
+        return false
+    }
+}
 
 function add(){
-    
+    if (isNumber(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+    }else{
+        window.alert('Valor invalido ou ja encontrado na lista')
+    }
 }
