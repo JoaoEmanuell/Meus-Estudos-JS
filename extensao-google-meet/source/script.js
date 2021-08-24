ae880c0c491aa792ec6362309f6e1da0a9117a82
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-08-24 16:16:10 -0300

    captura de mensagens funcionando

--- extensao-google-meet/source/script.js
+++ extensao-google-meet/source/script.js
@@ -1,3 +1,22 @@
 const interval = setInterval(() => {
-    
-}, 1000);
+    main();
+}, 10000);
+
+function main(){
+    let mens = mensages();
+    let area = text_area();
+    if (mens.length != 0) {
+        
+    }else{
+    }
+}
+
+function mensages(){
+    let men = document.querySelectorAll(".oIy2qc");
+    return men;
+}
+
+function text_area(){
+    let area = document.querySelector("textarea");
+    return area;
+}
