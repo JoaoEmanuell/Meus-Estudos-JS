c67120c3fd3ccff39b88f27851e6ca162344a524
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-09-14 14:45:20 -0300

    validRPA concluido

--- extensao-google-meet/source/JS/mathCommands.class.js
+++ extensao-google-meet/source/JS/mathCommands.class.js
@@ -6,15 +6,17 @@ class mathCommands{
         this.main();
     }
     main(){
+        // P.A
         if (this._el.indexOf(`${mathPrefix}p.a`) >= 0){
             if (this._el.indexOf(`${mathPrefix}p.a cl`) >= 0){
                 mensage(this.validateClassicPA(this._el));
             } else if(this._el.indexOf(`${mathPrefix}p.a n`) >= 0){
                 mensage(this.validNPA(this._el));
+            } else if(this._el.indexOf(`${mathPrefix}p.a r`) >= 0){
+                mensage(this.validRPA(this._el));
             }
         }
     }
-    // P.A
     // Validates
     validateClassicPA(el){
         this._el = String(el).replace(`${mathPrefix}p.a cl `, ``);
@@ -54,6 +56,26 @@ class mathCommands{
         } else{
             const classPa = new Math_PA();
             return classPa.NPA(listNumbers[0], listNumbers[1], listNumbers[2]);
+        }
+    }
+    validRPA(el){
+        this._el = String(el).replace(`${mathPrefix}p.a r `, ``);
+        let tmp = '';
+        let listNumbers = [];
+        for (let e = 0; e < this._el.length; e++){
+            tmp += this._el[e];
+            if (this._el[e] == ' ' || this._el.length === e +1){
+                if(!isNaN(tmp)){
+                    listNumbers.push(Number(tmp));
+                    tmp = '';
+                }
+            }
+        }
+        if (listNumbers.length != 3){
+            return `Algum numero invalido foi passado, tente novamente!`;
+        } else{
+            const classPa = new Math_PA();
+            return classPa.RPA(listNumbers[0], listNumbers[1], listNumbers[2]);
         }
     }
 }
