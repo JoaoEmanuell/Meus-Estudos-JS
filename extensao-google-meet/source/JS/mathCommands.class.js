3ad39dd97c642efb4b1b669f147e7500c9cde527
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-09-14 14:55:42 -0300

    validSPA

--- extensao-google-meet/source/JS/mathCommands.class.js
+++ extensao-google-meet/source/JS/mathCommands.class.js
@@ -14,6 +14,8 @@ class mathCommands{
                 mensage(this.validNPA(this._el));
             } else if(this._el.indexOf(`${mathPrefix}p.a r`) >= 0){
                 mensage(this.validRPA(this._el));
+            } else if(this._el.indexOf(`${mathPrefix}p.a s`) >= 0){
+                mensage(this.validSPA(this._el));
             }
         }
     }
@@ -76,6 +78,26 @@ class mathCommands{
         } else{
             const classPa = new Math_PA();
             return classPa.RPA(listNumbers[0], listNumbers[1], listNumbers[2]);
+        }
+    }
+    validSPA(el){
+        this._el = String(el).replace(`${mathPrefix}p.a s `, ``);
+        let tmp = '';
+        let listNumbers = [];
+        for (let e = 0; e < this._el.length; e++){
+            tmp += this._el[e];
+            if (this._el[e] == ' ' || this._el.length === e +1){
+                if(!isNaN(tmp)){
+                    listNumbers.push(Number(tmp));
+                    tmp = '';
+                }
+            }
+        }
+        if (listNumbers.length != 3){
+            return `Algum numero invalido foi passado, tente novamente!`;
+        } else{
+            const classPa = new Math_PA();
+            return classPa.SPA(listNumbers[0], listNumbers[1], listNumbers[2]);
         }
     }
 }
