c97ffd1ae54e3edf275fa5fd38bbe5e4086069b9
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-09-14 21:39:29 -0300

    melhoria do código e help mathCommands

--- extensao-google-meet/source/JS/mathCommands.class.js
+++ extensao-google-meet/source/JS/mathCommands.class.js
@@ -6,6 +6,19 @@ class mathCommands{
         this.main();
     }
     main(){
+        // PA
+        const pa = new PA(this._el);
+        // HELP
+        const help = new helpMath(this._el);
+    }
+}
+
+class PA{
+    constructor (el){
+        this._el = String(el).toLowerCase();
+        this.main();
+    }
+    main(){
         // P.A
         if (this._el.indexOf(`${mathPrefix}p.a`) >= 0){
             if (this._el.indexOf(`${mathPrefix}p.a cl`) >= 0){
@@ -98,5 +111,34 @@ class mathCommands{
             const classPa = new Math_PA();
             return classPa.SPA(listNumbers[0], listNumbers[1], listNumbers[2]);
         }
+    }
+}
+
+class helpMath{
+    constructor(el){
+        this._el = String(el).toLowerCase();
+        if (this._el.indexOf(`${mathPrefix}help`) === 0 || this._el.indexOf(`${mathPrefix}h`) === 0 ){
+            this._el = String(el).toLowerCase().replace(`${mathPrefix}help `, '').replace(`${mathPrefix}h `, '');
+            this.main();
+        }
+    }
+    main(){
+            switch (this._el){
+                case "1":
+                    this.pag1();
+                    break;
+                default:
+                    mensage(`Pagina invalida, digite "${mathPrefix}help pagina" ou "${mathPrefix}h pagina", as paginas vão até a pagina numero 1`);
+                    setSpanTime(3);
+            }
+    }
+    // Pages
+
+    pag1(){
+        mensage(`"${mathPrefix}p.a cl primeiro-numero razão numero-de-termos" retorna uma p.a classica, escrevendo todos os termos possiveis. Exemplo: "${mathPrefix}p.a cl 2 2 10" => "2, 4, 6...
+        \n"${mathPrefix}p.a n primeiro-numero razão numero-do-ultimo-termo" retorna o valor do ultimo termo da p.a. Exemplo: "${mathPrefix}p.a n 2 2 10" => 20`)
+        mensage(`"${mathPrefix}p.a r primeiro-numero numero-de-termos valor-do-ultimo-termo" retorna a razão da P.A Exemplo: "${mathPrefix}p.a r 2 10 20" => 2
+        \n"${mathPrefix}p.a s primeiro-termo valor-do-ultimo-termo numero-de-termos" Retorna a soma dos termos de uma P.A Exemplo: "${mathPrefix}p.a s 2 20 10"`);
+        setSpanTime(15)
     }
 }
