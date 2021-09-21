9cee56540b26e7ca690e37f4376a809a152e5ee4
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-09-21 20:25:02 -0300

    docstrings eq2.class.js

--- extensao-google-meet/source/JS/MATH/eq2.class.js
+++ extensao-google-meet/source/JS/MATH/eq2.class.js
@@ -1,16 +1,24 @@
+/**
+ * Class for Second degree equation
+ */
 class EQ2{
     constructor (el){
         this._el = String(el).toLowerCase();
         this.main();
     }
-
+/**
+ * main method
+ */
     main(){
         if (this._el.indexOf(`${mathPrefix}eq2`) >= 0){
             mensage(this.validateEq2());
         }
         
     }
-
+/**
+ * Validates whether the numbers passed match a quadratic equation.
+ * @returns {String} If the numbers passed are valid it will return the value of Delta, x' and x'' otherwise it will return an error message.
+ */
     validateEq2(){
         const men = this._el.replace(`${mathPrefix}eq2 `, ``);
         console.log(men)
