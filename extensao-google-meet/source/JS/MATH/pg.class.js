4223c4ffcd1ec58ca8d91ace459d5bf1e7284a56
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-09-22 17:31:03 -0300

    docstrings PG

--- extensao-google-meet/source/JS/MATH/pg.class.js
+++ extensao-google-meet/source/JS/MATH/pg.class.js
@@ -4,6 +4,7 @@ class PG{
         this.main();
     }
     main(){
+        // P.G
         if (this._el.indexOf(`${mathPrefix}p.g`) >= 0){
             if (this._el.indexOf(`${mathPrefix}p.g cl`) >= 0){
                 mensage(this.validateClassicPG(this._el));
@@ -16,6 +17,13 @@ class PG{
             }
         }
     }
+    // Validates
+    
+    /**
+     * Checks if the string has valid numbers to perform an operation that aims to return the numbers of a P.G, must be informed a1 = value of the first term, q = ratio, an = last term.
+     * @param {String} el String
+     * @returns if the message informs the parameters correctly it will return to P.G if not it will not return an error message.
+     */
     validateClassicPG(el){
         this._el = String(el).replace(`${mathPrefix}p.g cl `, ``);
         const listNumbers = ExtractNumbers(this._el, ' ');
@@ -26,6 +34,11 @@ class PG{
             return classPg.classicPG(listNumbers[0], listNumbers[1], listNumbers[2]);
         }
     }
+    /**
+     * Checks if the string has valid numbers to perform an operation that aims to return the value of the informed term of a P.G, must be informed a1 = value of the first term, q = reason, an = term that you want to know the value.
+    * @param {String} el String
+    * @returns if the message informs the parameters correctly it will return the value of the term informed P.G otherwise it will return an error message.
+     */
     validNPG(el){
         this._el = String(el).replace(`${mathPrefix}p.g n `, ``);
         const listNumbers = ExtractNumbers(this._el, ' ');
@@ -36,6 +49,11 @@ class PG{
             return classPg.NPG(listNumbers[0], listNumbers[1], listNumbers[2]);
         }
     }
+    /**
+     * Checks if the string has valid numbers to perform an operation that aims to return the value of the ratio of a P.A, must be informed a1 = value of the first term, a2 = value of the second term.
+    * @param {String} el String
+    * @returns if the message informs the parameters correctly it will return the P.G reason otherwise it will return an error message.
+     */
     validRPG(el){
         this._el = String(el).replace(`${mathPrefix}p.g r `, ``);
         const listNumbers = ExtractNumbers(this._el, ' ');
@@ -46,6 +64,11 @@ class PG{
             return classPg.RPG(listNumbers[0], listNumbers[1], listNumbers[2]);
         }
     }
+    /**
+     * Checks if the string has valid numbers to perform an operation that aims to return the sum of the values of a P.G, must be informed a1 = value of the first term, q = ratio, n number of terms
+    * @param {String} el String
+    * @returns if the message informs the parameters correctly it will return the sum of the P.G otherwise it will return an error message.
+    */
     validSPG(el){
         this._el = String(el).replace(`${mathPrefix}p.g s `, ``);
         const listNumbers = ExtractNumbers(this._el, ' ');
