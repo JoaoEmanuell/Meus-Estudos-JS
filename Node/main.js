fe5e68e7fc5cc6d615b86bec6ded125a9f3f7104
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-01-19 17:16:02 -0300

    body-parser

--- Node/main.js
+++ Node/main.js
@@ -3,8 +3,14 @@ const app = express();
 const connection = require("./source/SQL/connection.js");
 const exphbs = require("express-handlebars");
 const handlebars = exphbs.create({ defaultLayout: "main" });
+const bodyParser = require("body-parser");
 
 // Config
+
+    // Body Parser
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+
     // Template Engine
         app.engine("handlebars", handlebars.engine);
         app.set("view engine", "handlebars");
@@ -20,7 +26,9 @@ app.get('/cad', function(req, res) {
 });
 
 app.post('/visualize', function(req, res) {
-    res.send('Form added');
+    const title = req.body.title;
+    const content = req.body.content;
+    res.send(`<p>Title : ${title}</p><p>Content : ${content}</p>`);
 });
 
 // Execute
