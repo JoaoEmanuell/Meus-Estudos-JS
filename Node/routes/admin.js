995461f8f07141ce30b4f6c1121e96c7ad7b6098
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-02-01 12:24:35 -0300

    addcategories

--- Node/routes/admin.js
+++ Node/routes/admin.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+require("../models/Category");
+const Category = mongoose.model("Categorys");
 
 router.get('/', (req, res) => {
     res.render('./admin/index');
@@ -10,10 +13,33 @@ router.get('/posts', (req, res) => {
 });
 
 router.get('/categories', (req, res) => {
-    res.render('./admin/categories');
+    Category.findAll({
+        order : [['date', 'desc']]
+    }).then((categories) => {
+        console.log(categories);
+        res.render('./admin/categories', {categories : categories});
+    });
 });
 
 router.get('/addcategories', (req, res) => {
     res.render('./admin/addcategories');
-})
+});
+
+router.post('/categories/new', (req, res) => {
+    const new_category = {
+        name : String(req.body.name).toUpperCase(),
+        slug : String(req.body.slug).toLowerCase().replace(' ', '-'),
+    }
+
+    new Category({
+        name : new_category.name,
+        slug : new_category.slug
+    }).save().then(() => {
+        res.redirect('/admin/categories');
+    }
+    ).catch(err => {
+        res.send(`Erro ao cadastrar categoria ${err}`);
+    }
+    );
+});
 module.exports = router;
